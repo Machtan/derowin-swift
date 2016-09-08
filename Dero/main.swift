933f1a51d35dab46d26079f5edc839cb41cd381d
Machtan <jako3047@gmail.com>
2016-09-08 17:58:07 +0200

    Initial Commit

---
+++ Dero/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Dero
+//
+//  Created by Jakob Lautrup Nysom on 08/09/16.
+//  Copyright © 2016 Jakob Lautrup Nysom. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
